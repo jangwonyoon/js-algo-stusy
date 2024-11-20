bc6bda4b5ffff906cc6a4e3cd3ac6deaee7b0c97
이승준 <nate199458@gmail.com>
2024-11-20 13:14:02 +0900

    sj : merge sorted

---
+++ 1Week/SJ/p1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1; // 2
+  let j = n - 1; // 2
+  let k = m + n - 1; // 3
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] >= nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+  return nums1;
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
